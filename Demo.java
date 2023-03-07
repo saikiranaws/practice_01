52aa7b3473148366f1078da377c1e755889d10d4
saikiranaws <saidevopsaws09@gmail.com>
2023-03-07 19:03:36 +0530

    Addition Method added by sai

---
+++ Demo.java
@@ -0,0 +1,16 @@
+class Sample
+{
+Public int addition(int a, int b)
+{
+int c=a+b;
+returen int;
+}
+}
+
+public class Demo{
+public static void main(String[] args)
+{
+Sample s=new Sample();
+s.addition(10,20);
+}
+}
